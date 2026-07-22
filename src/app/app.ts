a963af1d8390db1d097bc0fd54d2eabd419705c8
Conner Klee <connerklee@icloud.com>
2026-07-22 23:01:50 +0200

    feat: add initial portfolio structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Header } from './layout/header/header';
+import { Footer } from './layout/footer/footer';
+import { Hero } from './sections/hero/hero';
+import { About } from './sections/about/about';
+import { Experience } from './sections/experience/experience';
+import { Skills } from './sections/skills/skills';
+import { Contact } from './sections/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, Hero, About, Experience, Skills, Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('portfolio');
-}
+export class App {}
---
+++ src/app/sections/hero/hero.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  templateUrl: './hero.html',
+  styleUrl: './hero.scss',
+})
+export class Hero {}
